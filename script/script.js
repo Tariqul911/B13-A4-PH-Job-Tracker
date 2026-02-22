a4f16ae46457cdb6d165473301d12dcb67f8c84f
Tariqul911 <zxtariqul911@gmail.com>
2026-02-23 01:43:59 +0600

    setup basic state and selected DOM elements

--- script/script.js
+++ script/script.js
@@ -91,3 +91,18 @@ const jobs = [
   },
 ];
 
+// --------- State ----------
+let currentTab = "All"; // "All" | "Interview" | "Rejected"
+
+// --------- Elements ----------
+const jobsContainer = document.getElementById("jobs-container");
+
+const totalCountEl = document.getElementById("total-count");
+const interviewCountEl = document.getElementById("interview-count");
+const rejectedCountEl = document.getElementById("rejected-count");
+
+const tabCountEl = document.getElementById("tab-count");
+
+const tabAllBtn = document.getElementById("tab-all");
+const tabInterviewBtn = document.getElementById("tab-interview"); 
+const tabRejectedBtn = document.getElementById("tab-rejected");
